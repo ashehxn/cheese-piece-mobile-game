44aba6ac37d50e2e8a4975c1bcb82de17124a040
Avishka Shehan <shehanwickramasooriya05@gmail.com>
2024-04-23 17:01:15 +0530

    Complete Level 1 Structure

--- app/src/main/java/com/example/cheesepiece/Level1.kt
+++ app/src/main/java/com/example/cheesepiece/Level1.kt
@@ -10,21 +10,15 @@ import android.util.Log
 
 class Level1 : AppCompatActivity() {
     companion object {
-        // Maximum number of grid cells horizontally and vertically
-        private const val MAX_GRID_WIDTH = 5 // Adjust according to your grid width
-        private const val MAX_GRID_HEIGHT = 5 // Adjust according to your grid height
+        private const val MAX_GRID_WIDTH = 5
+        private const val MAX_GRID_HEIGHT = 5
     }
 
-    // Variables to store initial touch position and offset
-    private var initialX = 0f
-    private var initialY = 0f
     private var offsetX = 0f
     private var offsetY = 0f
 
-    // Size of each grid cell
     private val gridSize by lazy { resources.getDimensionPixelSize(R.dimen.box_size) }
 
-    // Variables to store positions of image views
     private var imageView1Position = mutableSetOf<Pair<Int, Int>>()
     private var imageView2Position = mutableSetOf<Pair<Int, Int>>()
     private var imageView3Position = mutableSetOf<Pair<Int, Int>>()
@@ -48,13 +42,39 @@ class Level1 : AppCompatActivity() {
         val imageView6 = findViewById<ImageView>(R.id.imageView6)
         val imageView7 = findViewById<ImageView>(R.id.imageView7)
 
-        imageView1Position = getCoveredGridCells(imageView1).toMutableSet()
-        imageView2Position = getCoveredGridCells(imageView2).toMutableSet()
-        imageView3Position = getCoveredGridCells(imageView3).toMutableSet()
-        imageView4Position = getCoveredGridCells(imageView4).toMutableSet()
-        imageView5Position = getCoveredGridCells(imageView5).toMutableSet()
-        imageView6Position = getCoveredGridCells(imageView6).toMutableSet()
-        imageView7Position = getCoveredGridCells(imageView7).toMutableSet()
+        imageView1Position = mutableSetOf(
+            Pair(0, 0),
+            Pair(1, 0)
+        )
+        imageView2Position = mutableSetOf(
+            Pair(2, 0),
+            Pair(2, 1)
+        )
+        imageView3Position = mutableSetOf(
+            Pair(0, 1),
+            Pair(0, 2),
+            Pair(0, 3),
+            Pair(0, 4)
+        )
+        imageView4Position = mutableSetOf(
+            Pair(1, 1),
+            Pair(1, 2),
+            Pair(1, 3)
+        )
+        imageView5Position = mutableSetOf(
+            Pair(1, 4),
+            Pair(2, 4),
+            Pair(3, 4),
+            Pair(4, 4)
+        )
+        imageView6Position = mutableSetOf(
+            Pair(3, 1),
+            Pair(4, 1)
+        )
+        imageView7Position = mutableSetOf(
+            Pair(3, 2),
+            Pair(3, 3)
+        )
 
 
         allImageViewPositions.addAll(imageView1Position)
@@ -65,7 +85,7 @@ class Level1 : AppCompatActivity() {
         allImageViewPositions.addAll(imageView6Position)
         allImageViewPositions.addAll(imageView7Position)
 
-        Log.d("Tag", allImageViewPositions.toString())
+        Log.d("allImageViewPositions1", allImageViewPositions.toString())
 
         setTouchListener(imageView1)
         setTouchListener(imageView2)
@@ -76,21 +96,14 @@ class Level1 : AppCompatActivity() {
         setTouchListener(imageView7)
     }
 
-    // Set touch listener for each image view
     private fun setTouchListener(view: View) {
         view.setOnTouchListener(touchListener)
     }
 
-    // Touch listener to handle moving and snapping of image views
-    // Touch listener to handle moving and snapping of image views
     private val touchListener = View.OnTouchListener { view, event ->
-        // Define overlappingCells here so it's accessible throughout the scope of the touchListener
-        var overlappingCells: Set<Pair<Int, Int>> = emptySet()
 
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
-                initialX = view.x
-                initialY = view.y
                 offsetX = event.rawX - view.x
                 offsetY = event.rawY - view.y
             }
@@ -98,9 +111,13 @@ class Level1 : AppCompatActivity() {
                 val newX = event.rawX - offsetX
                 val newY = event.rawY - offsetY
 
-                // Check if the move is within bounds
+                val tempX = newX
+                val tempY = newY
+
                 if (isWithinBounds(newX, newY, view.width, view.height)) {
-                    // Check if the move overlaps with occupied grid cells
+                    view.x = newX
+                    view.y = newY
+
                     val newCoveredCells = getCoveredGridCells(view)
 
                     when (view) {
@@ -112,58 +129,42 @@ class Level1 : AppCompatActivity() {
                         findViewById<ImageView>(R.id.imageView6) -> imageView6Position = newCoveredCells.toMutableSet()
                         findViewById<ImageView>(R.id.imageView7) -> imageView7Position = newCoveredCells.toMutableSet()
                     }
+                    view.x = tempX
+                    view.y = tempY
 
+                    allImageViewPositions.clear()
+                    allImageViewPositions.addAll(imageView1Position)
+                    allImageViewPositions.addAll(imageView2Position)
+                    allImageViewPositions.addAll(imageView3Position)
+                    allImageViewPositions.addAll(imageView4Position)
+                    allImageViewPositions.addAll(imageView5Position)
+                    allImageViewPositions.addAll(imageView6Position)
+                    allImageViewPositions.addAll(imageView7Position)
 
-
-
-                    overlappingCells = newCoveredCells.intersect(allImageViewPositions)
-
-                    // If there are no overlapping cells, allow the move
-                    if (overlappingCells.isEmpty()) {
-                        view.x = newX
-                        view.y = newY
-
-                        allImageViewPositions.clear()
-                        allImageViewPositions.addAll(imageView1Position)
-                        allImageViewPositions.addAll(imageView2Position)
-                        allImageViewPositions.addAll(imageView3Position)
-                        allImageViewPositions.addAll(imageView4Position)
-                        allImageViewPositions.addAll(imageView5Position)
-                        allImageViewPositions.addAll(imageView6Position)
-                        allImageViewPositions.addAll(imageView7Position)
-                    }
                 }
             }
             MotionEvent.ACTION_UP -> {
                 snapToGridPosition(view)
-                // Clear overlappingCells here after the move is complete
-                overlappingCells = emptySet()
             }
         }
         true
     }
 
-
-
     private fun snapToGridPosition(view: View) {
         val newGridX = ((view.x + gridSize / 2) / gridSize).toInt()
         val newGridY = ((view.y + gridSize / 2) / gridSize).toInt()
 
-        // Calculate the actual position after snapping to the grid
         val snappedX = newGridX * gridSize
         val snappedY = newGridY * gridSize
 
-        // Snap to the nearest grid position
         view.x = snappedX.toFloat()
         view.y = snappedY.toFloat()
     }
 
-    // Check if the view's new position is within the bounds of the grid
     private fun isWithinBounds(x: Float, y: Float, width: Int, height: Int): Boolean {
         return x >= 0 && y >= 0 && x + width <= MAX_GRID_WIDTH * gridSize && y + height <= MAX_GRID_HEIGHT * gridSize
     }
 
-    // Function to get all grid cells covered by an ImageView
     private fun getCoveredGridCells(view: View): Set<Pair<Int, Int>> {
         val parent = view.parent as? GridLayout ?: return emptySet()
         val cellWidth = parent.width / parent.columnCount
@@ -176,8 +177,8 @@ class Level1 : AppCompatActivity() {
         val endRow = ((view.y + view.height) / cellHeight).toInt()
 
         val coveredCells = mutableSetOf<Pair<Int, Int>>()
-        for (i in startColumn..endColumn-1) {
-            for (j in startRow..endRow-1) {
+        for (i in startColumn..<endColumn) {
+            for (j in startRow..<endRow) {
                 coveredCells.add(Pair(i, j))
             }
         }
